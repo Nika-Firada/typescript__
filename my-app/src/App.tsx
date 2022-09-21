706df2f67aa4079e23393ce0bc943601fd88fdda
Nika-Firada <nika.firada@gmail.com>
2022-09-22 00:19:10 +0400

    hard

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { CharacterInformation } from './CharacterInformation';
+import { CharacterType, fetchCharacter } from './characters';
+import { Loading } from './Loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type WithCharacterProps = {
+  character: CharacterType;
+};
+
+function withCharacter<T extends WithCharacterProps>(
+  Component: React.ComponentType<T>
+) {
+  return (props: Omit<T, keyof WithCharacterProps>) => {
+    const [character, setCharacter] = React.useState<CharacterType | null>(
+      null
+    );
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+      fetchCharacter().then((c) => {
+        setCharacter(c);
+        setLoading(false);
+      });
+    }, []);
+
+    if (loading) return <Loading />;
+    return character && <Component {...(props as T)} character={character} />;
+  };
 }
+
+const App = () => {
+  const CharacterInformationWithCharacter = withCharacter(CharacterInformation);
+
+  return (
+    <main>
+      <CharacterInformationWithCharacter />
+    </main>
+  );
+};
 
 export default App;
